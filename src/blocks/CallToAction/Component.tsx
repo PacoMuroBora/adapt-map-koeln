0381c25a7959229dcd0db38e2fcdb00dbba08b2c
MarSchiff <M.Schiffeler@gmail.com>
2026-03-04 10:10:10 +0100

    CTA styling update

--- src/blocks/CallToAction/Component.tsx
+++ src/blocks/CallToAction/Component.tsx
@@ -7,8 +7,8 @@ import { CMSLink } from '@/components/Link'
 
 export const CallToActionBlock: React.FC<CTABlockProps> = ({ links, richText }) => {
   return (
-    <div className="container h-screen flex items-center justify-center md:h-[640px] bg-am-dark background-grid-dark p-4 md:p-8 lg:p-16">
-      <div className="w-full min-h-[66%] md:h-full bg-primary rounded-2xl px-4 md:px-8 py-8 lg:p-16 space-y-8">
+    <div className="container h-screen md:h-auto flex items-center justify-center p-0 m-0 md:p-8 lg:p-16">
+      <div className="w-full h-full flex flex-col justify-center bg-primary background-grid-primary md:rounded-2xl px-4 md:px-8 py-8 lg:p-16 space-y-8">
         <div className="max-w-[40rem] flex items-center">
           {richText && <RichText className="mb-0" data={richText} enableGutter={false} />}
         </div>
