f8d12a7cc5d1525553aebcf2f8534883ffc21fc2
MarSchiff <M.Schiffeler@gmail.com>
2026-03-09 14:19:00 +0100

    fix container styles

--- src/blocks/CallToAction/Component.tsx
+++ src/blocks/CallToAction/Component.tsx
@@ -12,7 +12,7 @@ export const CallToActionBlock: React.FC<CTABlockProps & { isLandingLayout?: boo
   richText,
 }) => {
   return (
-    <div className="container pt-4 md:py-8 lg:py-16">
+    <div className="container-card pt-4 md:py-8 lg:py-16">
       <Card
         variant="primary"
         className="inner-container h-screen md:h-auto flex flex-col justify-center py-8 md:py-12 lg:py-20 rounded-none md:rounded-3xl"
